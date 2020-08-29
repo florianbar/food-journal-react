ada2ff34c9c5af2f063ec39a99376bf7fdbb7569
florianbar <florianbar01@gmail.com>
2020-08-29 17:50:17 +0200

    Added form

--- src/containers/RecordMeasurements/RecordMeasurements.js
+++ src/containers/RecordMeasurements/RecordMeasurements.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 
+import Button from '../../components/UI/Button/Button';
+
 class RecordMeasurements extends Component {
+    state = {
+        form: {
+            weight: {
+                value: 71
+            },
+            bodyFat: {
+                value: 13.3
+            }
+        }
+    }
+
+    formSubmitHandler = (event) => {
+        event.preventDefault();
+
+        console.log("[formSubmitHandler]", this.state.form);
+    }
+
+    inputChangedHandler = (event, inputName) => {
+        console.log("[inputChangedHandler]", event.target.value);
+
+        const updatedForm = {...this.state.form};
+        const updatedFormElement = {...updatedForm[inputName]};
+        updatedFormElement.value = event.target.value;
+        updatedForm[inputName] = updatedFormElement;
+
+        this.setState({
+            form: updatedForm
+        });
+    }
+
     render () {
         return (
-            <div>
+            <form onSubmit={this.formSubmitHandler}>
                 <h1>Record Measurements</h1>
-            </div>
+
+                <label>Weight:</label>
+                <input 
+                    type="number" 
+                    name="weight" 
+                    value={this.state.form.weight.value}
+                    onChange={(event) => this.inputChangedHandler(event, "weight")} />
+
+                <label>Body Fat:</label>
+                <input 
+                    type="number" 
+                    name="bodyFat" 
+                    value={this.state.form.bodyFat.value}
+                    onChange={(event) => this.inputChangedHandler(event, "bodyFat")} />
+
+                <Button btnType="Success">Submit</Button>
+            </form>
         );
     }
 };
